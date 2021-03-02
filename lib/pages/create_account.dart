df13df76c2109c02772aa6358825f0ff4a3d4352
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-02 19:28:46 -0400

    Update autovalidate to autovalidateMode

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -45,7 +45,7 @@ class _CreateAccountState extends State<CreateAccount> {
                 child: Container(
                   child: Form(
                     key: _formKey,
-                    autovalidate: true,
+                    autovalidateMode: AutovalidateMode.always,
                     child: TextFormField(
                       validator: (val) {
                         if (val.trim().length < 3 || val.isEmpty) {
